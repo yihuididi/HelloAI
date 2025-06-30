39c21136c0fb1307c1b012752a83e11ade07a8e1
yihuididi <tohyihui11@yahoo.com>
2025-06-30 18:25:38 +0800

    Initialize project

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import express from 'express';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('Server started at http://localhost:' + PORT);
+});
